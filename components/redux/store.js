fd236495497370c5b0f4050ed166e015f81f3b8f
Jyoti Shukla <jyoti.s@Jyotis-MacBook-Air.local>
2025-07-05 16:16:13 +0530

    Initial commit
    Generated by react-native@0.80.1

--- components/redux/store.js
+++ components/redux/store.js
@@ -1,7 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+const persistConfig={
+key: 'root',
+storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
     reducer : rootReducer
 });
+export const persistor = persistStore(store);
 export default store;
