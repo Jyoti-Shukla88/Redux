4b390e3f1742e1416c18780096b763aca5f19604
Jyoti-Shukla88 <shuklajyoti0811@gmail.com>
2025-07-09 17:00:16 +0530

    Add files via upload

---
+++ FirstApp/src/screens/RefferalCode.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RefferalCode = () => {
+  return (
+    <View>
+      <Text>RefferalCode</Text>
+    </View>
+  )
+}
+
+export default RefferalCode
+
+const styles = StyleSheet.create({})
